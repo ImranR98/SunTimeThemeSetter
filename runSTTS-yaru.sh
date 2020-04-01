1ef6709e377360307df741b6f068b19e02e24f45
Imran Remtulla <iremtulla15@gmail.com>
2020-04-01 19:21:03 -0400

    Add yaru script for NUC

---
+++ runSTTS-yaru.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+FILESFOLDER="$(ls ~ | grep "^STTS-Files$")"
+if [ -z "$FILESFOLDER" ]
+then
+			echo "Creating Files folder..."
+			mkdir ~/STTS-Files
+fi
+
+while [ true ]; do node "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/SunTimeThemeSetter.js Yaru Yaru-dark &> ~/STTS-Files/STTSLastActionLog.txt; sleep 60; done;

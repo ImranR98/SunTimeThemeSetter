ff76e49ba8967dd3c72c3f36d34e782f0b283262
Imran Remtulla <iremtulla15@gmail.com>
2020-04-13 15:37:19 -0400

    Using sun.js library instead of API for sunrise/sunset

--- SunTimeThemeSetter.js
+++ SunTimeThemeSetter.js
@@ -1,23 +1,26 @@
 // This Node App uses 3 free APIs to get the local sunrise/sunset times, then sets a light/dark gnome-theme.
 // If a Mailspring folder exists in the current User's ~/.config then the *.core.theme property in config.json is changed accordingly (ui-light or ui-dark)
 // Mailspring themes are hardcoded for now
+// Imran Remtulla
 
 const https = require('https')
 const bashSync = require('child_process').execSync
 const fs = require('fs')
 const bash = require('child_process').exec
 
+require('./sun')
+
 // Hardcoded vars.
 const savedTimesPath = '/home/' + bashSync('whoami').toString().trim() + '/STTS-Files/savedSunTimes.json'
 const lastSetThemesPath = '/home/' + bashSync('whoami').toString().trim() + '/STTS-Files/lastSetThemes.json'
-const mailSpringDarkTheme = 'ui-dark';
-const mailSpringLightTheme = 'ui-light';
+const mailSpringDarkTheme = 'ui-dark'
+const mailSpringLightTheme = 'ui-light'
 
 // Pause code execution for specified ms
-let sleep = (ms) => new Promise((resolve, reject) => { setTimeout(() => { resolve() }, ms) })
+const sleep = (ms) => new Promise((resolve, reject) => { setTimeout(() => { resolve() }, ms) })
 
 // Simple HTTP Get
-let simpleHttpGet = (url) => {
+const simpleHttpGet = (url) => {
 	return new Promise((resolve, reject) => {
 		https.get(url, (resp) => {
 			let data = ''
@@ -36,7 +39,7 @@ let simpleHttpGet = (url) => {
 }
 
 // Try simpleHttpGet 3 times in case of failure, with a 5000 ms gap between tries
-let simpleHttpGet3Tries = async (url, tried = 1) => {
+const simpleHttpGet3Tries = async (url, tried = 1) => {
 	try {
 		let res = await simpleHttpGet(url)
 		return res
@@ -50,21 +53,30 @@ let simpleHttpGet3Tries = async (url, tried = 1) => {
 	}
 }
 
+// Takes a Date and returns it as an int (hours + minutes)
+const getTimeNumberFromDate = (date) => {
+	let hr = date.getHours().toString()
+	let min = date.getMinutes().toString()
+	if (min.length == 1) {
+		min = '0' + min
+	}
+	return Number.parseInt(hr + min)
+}
+
 // Use free online APIs to get your IP, then IP-based location, then location based sunrise/sunset times
-// Returns an object of { sunrise, sunset, time }
+// Returns an object of { sunrise, sunset, calculatedTime }
 // Saves above object to a file if a valid path is given
-let getNewSunTimes = async (saveToFilePath = null) => {
+const getNewSunTimes = async (saveToFilePath = null) => {
 	console.log('Getting new Sun Times data...')
 	console.log('Getting public IP...')
 	let publicIP = await simpleHttpGet3Tries('https://ipinfo.io/ip')
 	console.log('Getting location from IP...')
 	let IPLocation = JSON.parse(await simpleHttpGet3Tries(`https://ipvigilante.com/${publicIP}`)).data
-	console.log('Getting Sun Times from Location...')
-	let { sunrise, sunset } = JSON.parse(await simpleHttpGet3Tries(`https://api.sunrise-sunset.org/json?lat=${IPLocation.latitude}&lng=${IPLocation.longitude}`)).results
+	console.log('Calculating Sun Times from Location...')
 	let final = {
-		sunrise: sunrise,
-		sunset: sunset,
-		time: new Date()
+		sunrise: getTimeNumberFromDate(new Date().sunrise(IPLocation.latitude, IPLocation.longitude)),
+		sunset: getTimeNumberFromDate(new Date().sunset(IPLocation.latitude, IPLocation.longitude)),
+		calculatedTime: new Date()
 	}
 	if (saveToFilePath) {
 		fs.writeFileSync(saveToFilePath, JSON.stringify(final, null, '\t'))
@@ -74,15 +86,15 @@ let getNewSunTimes = async (saveToFilePath = null) => {
 }
 
 // Gets the sunrise/sunset times either from a file or the getNewSunTimes function
-// If a valid file path is specified for a JSON file for a { sunrise, sunset, time } object, the object is used
-// If the time variable above is older than staleDataHoursLimit hours, getNewSunTimes is used to load new data and replace the existing file
-let getSunTimes = async (sunTimesFilePath, staleDataHoursLimit = 24) => {
+// If a valid file path is specified for a JSON file for a { sunrise, sunset, calculatedTime } object, the object is used
+// If the calculatedTime variable above is older than staleDataHoursLimit hours, getNewSunTimes is used to load new data and replace the existing file
+const getSunTimes = async (sunTimesFilePath, staleDataHoursLimit = 24) => {
 	let sunTimes = null
 	if (sunTimesFilePath) {
 		if (fs.existsSync(sunTimesFilePath)) {
 			try {
 				let data = JSON.parse(fs.readFileSync(sunTimesFilePath))
-				if (data.sunrise && data.sunset && data.time) {
+				if (data.sunrise && data.sunset && data.calculatedTime) {
 					sunTimes = data
 				} else {
 					throw 'Saved Sun Time Data in the file is incomplete.'
@@ -95,7 +107,7 @@ let getSunTimes = async (sunTimesFilePath, staleDataHoursLimit = 24) => {
 		}
 	}
 	if (sunTimes) {
-		if (((new Date() - new Date(sunTimes.time)) / 1000 / 60 / 60) > staleDataHoursLimit) {
+		if (((new Date() - new Date(sunTimes.calculatedTime)) / 1000 / 60 / 60) > staleDataHoursLimit) {
 			console.log(`Saved Sun Times data is older than ${staleDataHoursLimit} Hours - will get newer Data.`)
 			sunTimes = null
 		}
@@ -106,7 +118,7 @@ let getSunTimes = async (sunTimesFilePath, staleDataHoursLimit = 24) => {
 	return sunTimes
 }
 
-let getLastSetThemes = async (lastSetThemesPath) => {
+const getLastSetThemes = async (lastSetThemesPath) => {
 	let lastSetThemes = {
 		gnome: null,
 		mailspring: null
@@ -132,14 +144,14 @@ let getLastSetThemes = async (lastSetThemesPath) => {
 }
 
 // Change the gnome theme
-let changeGNOMETheme = (theme) => {
-	bashSync(`gsettings set org.gnome.shell.extensions.user-theme name "${theme}"; gsettings set org.gnome.desktop.interface gtk-theme "${theme}"`)
+const changeGNOMETheme = (theme) => {
+	bashSync(`gsettings set org.gnome.shell.extensions.user-theme name "${theme}" && gsettings set org.gnome.desktop.interface gtk-theme "${theme}"`)
 	let time = new Date()
 	console.log(`GNOME Theme ${theme} set at ${time.getHours()}:${time.getMinutes()}:${time.getMilliseconds()}.`)
 }
 
 // If a Mailspring folder exists in the current User's ~/.config, then the *.core.theme property in config.json is changed to the theme provided
-let changeMailSpringTheme = (theme) => {
+const changeMailSpringTheme = (theme) => {
 	let username = bashSync('whoami').toString().trim()
 	if (fs.existsSync(`/home/${username}/.config/Mailspring/config.json`)) {
 		let mailspringConfig = JSON.parse(fs.readFileSync(`/home/${username}/.config/Mailspring/config.json`).toString())
@@ -165,10 +177,10 @@ let changeMailSpringTheme = (theme) => {
 }
 
 // Takes an object of { sunrise, sunset } and uses it to set a dark or light theme
-let changeThemesWithSunTimes = (sunTimes, lastSetThemes, lightTheme, darkTheme) => {
-	let now = getCurrentTimeNumber()
-	let sunrise = convertTimeStringToNumber(sunTimes.sunrise, true)
-	let sunset = convertTimeStringToNumber(sunTimes.sunset, true)
+const changeThemesWithSunTimes = (sunTimes, lastSetThemes, lightTheme, darkTheme) => {
+	let now = getTimeNumberFromDate(new Date())
+	let sunrise = sunTimes.sunrise
+	let sunset = sunTimes.sunset
 	let time = new Date()
 	if (now > sunrise && now < sunset) {
 		if (lightTheme != lastSetThemes.gnome) {
@@ -200,45 +212,10 @@ let changeThemesWithSunTimes = (sunTimes, lastSetThemes, lightTheme, darkTheme) 
 	fs.writeFileSync(lastSetThemesPath, JSON.stringify(lastSetThemes, null, '\t'))
 }
 
-// Takes a time string ('HH:MM PM') and changes it to an int for easy comparison
-// If subtractUTCOffset is true, the local time zone offset is accounted for 
-let convertTimeStringToNumber = (timeString, subtractUTCOffset = false) => {
-	let timeTokens = timeString.trim().split(':')
-	timeTokens[0] = Number.parseInt(timeTokens[0])
-	if (timeTokens[2].indexOf('PM') != -1) {
-		if (timeTokens[0] != 12) {
-			timeTokens[0] += 12
-		}
-	} else {
-		if (timeTokens[0] == 12) {
-			timeTokens[0] += 0
-		}
-	}
-	if (subtractUTCOffset) {
-		timeTokens[0] -= (new Date().getTimezoneOffset() / 60)
-	}
-	timeTokens[0] = timeTokens[0].toString()
-	if (timeTokens[1].length == 1) {
-		timeTokens[1] = '0' + timeTokens[1]
-	}
-	return Number.parseInt(timeTokens[0] + timeTokens[1])
-}
-
-// Gets the loca time and returns it as an int (hours + minutes)
-let getCurrentTimeNumber = () => {
-	let temp = new Date()
-	let hr = temp.getHours().toString()
-	let min = temp.getMinutes().toString()
-	if (min.length == 1) {
-		min = '0' + min
-	}
-	return Number.parseInt(hr + min)
-}
-
 // Main logic
 // Gets the sunrise/sunset times and sets the Gnome-theme accordingly.
 // Themes are from the program arguments (process.argv[2] and process.argv[3])
-let run = async () => {
+const run = async () => {
 	if (process.argv.length <= 3) {
 		throw 'Please provide light and dark theme names as arguments!'
 	} else {

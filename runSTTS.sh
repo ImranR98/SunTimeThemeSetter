85d7769984d177a1fb1aaa31040c4876bd5e6ac9
Imran Remtulla <iremtulla15@gmail.com>
2020-04-22 01:13:33 -0400

    Removed adapta

--- runSTTS.sh
+++ runSTTS.sh
@@ -6,4 +6,4 @@ then
 			mkdir ~/STTS-Files
 fi
 
-while [ true ]; do node "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/SunTimeThemeSetter.js Adapta-RedGrey Adapta-RedGrey-Nokto &> ~/STTS-Files/STTSLastActionLog.txt; sleep 60; done;
+while [ true ]; do node "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/SunTimeThemeSetter.js Yaru Yaru-dark &> ~/STTS-Files/STTSLastActionLog.txt; sleep 60; done;

5019ea7dd92087d6306f25f2e21c212ee3db23a1
ImranR98 <30463115+ImranR98@users.noreply.github.com>
2020-05-21 14:04:22 -0400

    Update runSTTS.sh

--- runSTTS.sh
+++ runSTTS.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
-FILESFOLDER="$(ls ~ | grep "^STTS-Files$")"
-if [ -z "$FILESFOLDER" ]
+LIGHTTHEME='Yaru-Red'
+DARKTHEME='Yaru-Red-dark'
+
+FILESFOLDER=~/STTS-Files # If you change this, change it in the .js too.
+if [ -d "$FILESFOLDER" ]
 then
 			echo "Creating Files folder..."
 			mkdir ~/STTS-Files
 fi
 
 while [ true ]; do
-	node "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/SunTimeThemeSetter.js Yaru-Red Yaru-Red-dark &> ~/STTS-Files/STTSLastActionLog.txt
+	node "$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"/SunTimeThemeSetter.js "$LIGHTTHEME" "$DARKTHEME" &> ~/STTS-Files/STTSLastActionLog.txt
 	sleep 60
 done;
